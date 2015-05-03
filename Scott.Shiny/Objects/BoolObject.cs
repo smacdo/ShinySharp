f654dda6e535489d41b22801dc22502b3125f7c7
Scott MacDonald <scott@smacdo.com>
2015-05-02 20:08:36 -0700

    Forgot to include the bool objects

---
+++ Scott.Shiny/Objects/BoolObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scott.Shiny.Objects
+{
+    /// <summary>
+    ///  Represents a singleton boolean value.
+    /// </summary>
+    public abstract class BoolObject : SingletonObject
+    {
+        /// <summary>
+        ///  Bool constructor.
+        /// </summary>
+        protected BoolObject()
+        {
+        }
+    }
+
+    /// <summary>
+    ///  True boolean value.
+    /// </summary>
+    public class TrueBoolObject : BoolObject
+    {
+        public override string ToString()
+        {
+            return "#t";
+        }
+    }
+
+
+    /// <summary>
+    ///  False boolean value.
+    /// </summary>
+    public class FalseBoolObject : BoolObject
+    {
+        public override string ToString()
+        {
+            return "#f";
+        }
+    }
+}
---
+++ Scott.Shiny/REPL/SessionContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scott.Shiny.Objects;
+
+namespace Scott.Shiny.REPL
+{
+    /// <summary>
+    ///  Contains information local to the current REPL session.
+    /// </summary>
+    public class SessionContext
+    {
+        /// <summary>
+        ///  Constructor. Initialize the session context.
+        /// </summary>
+        public SessionContext()
+        {
+            True = new TrueBoolObject();
+            False = new FalseBoolObject();
+        }
+
+        /// <summary>
+        ///  Get the true bool singleton.
+        /// </summary>
+        public TrueBoolObject True { get; private set; }
+
+        /// <summary>
+        ///  Get the false bool singleton.
+        /// </summary>
+        public FalseBoolObject False { get; private set; }
+    }
+}
